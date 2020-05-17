3f28be61f1584a900985c4152502e94280b4c13c
hassan azizi <hassanazizi@hassans-MacBook-Air.local>
2020-05-17 10:00:41 -0400

    commit add

---
+++ src/test/java/com/cybertek/tests/day1_Navigation/VrifyURLOfPracticeWeb.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day1_Navigation;
+
+public class VrifyURLOfPracticeWeb {
+
+    public static void main(String[] args) {
+        /*
+        1. go to cybertek practice website
+                http://practice.cybertekschool.com/
+        2. verify the URL
+         */
+
+
+
+    }
+}
---
+++ src/test/java/com/cybertek/tests/day8_review_2/GetAttributeExamples.java
@@ -0,0 +1,9 @@
+package com.cybertek.tests.day8_review_2;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GetAttributeExamples {
+}
